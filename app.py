b193d26508f9439ff948907b62e63ed99ae859dd
mikellagreen <mag72@duke.edu>
2018-08-08 15:35:27 -0700

    Added redirects in nav bar

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask #this is our backend microframework
+from flask import render_template #handy dandy ext to render our html layout
+from flask_pymongo import PyMongo #flask specific bridge to mongodb driver (pymongo)
+
+
 
 app = Flask(__name__)
 
 @app.route("/")
 def index(): 
     return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port=5000,debug=True)
