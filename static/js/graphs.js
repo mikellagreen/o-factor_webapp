2de95058b35a59d782c08e69b09e639bff9b95c5
mikellagreen <mag72@duke.edu>
2018-08-09 17:20:58 -0700

    trying graph

--- static/js/graphs.js
+++ static/js/graphs.js
@@ -5,23 +5,42 @@ d3.queue()
 
 function makeGraphs(error, dataJSON){
     var data = dataJSON;
+    var alldata = allJSON;
     //crossfilter instance
     var ndx = crossfilter(data);
 
     // define data dimensions 
-    var aDim = ndx.dimension(function(d) { return d["a"]; });
-    var bDim = ndx.dimension(function(d) { return d["b"]; });
-    var cDim = ndx.dimension(function(d) { return d["c"]; });
-    var dDim = ndx.dimension(function(d) { return d["d"]; });
+    var yearDim = ndx.dimension(function(d) { return d["fake_year"]; });
+    var journalDim = ndx.dimension(function(d) { return d["fake_journal"]; });
+    var codeSharedDim = ndx.dimension(function(d) { return d["code_shared"]; });
+    var dataSharedDim = ndx.dimension(function(d) { return d["data_shared"]; });
+    var preprintDim = ndx.dimension(function(d) { return d["preprint"]; });
     
     // define data groups
     var all = ndx.groupAll();
-    var numJournalsByA = aDim.group();
+    var byJournal = journalDim.group();
     var numJournalsByB = bDim.group();
     var numJournalsByC = cDim.group();
     var numJournalsByD = dDim.group();
 
+    var minDate = yearDim.bottom(1)[0]["fake_year"];
+    var maxDate = yearDim.top(1)[0]["fake_year"];
 
+    var timeChart = dc.barChart("#time-chart");
+
+    timeChart
+    .width(600)
+    .height(160)
+    .margins({top: 10, right: 50, bottom: 30, left: 50})
+    .dimension(yearDim)
+    .group(byJournal)
+    .transitionDuration(500)
+    .x(d3.time.scale().domain([minDate, maxDate]))
+    .elasticY(true)
+    .xAxisLabel("Year")
+    .yAxis().ticks(4);
+
+    dc.renderAll();
     //define types of charts
-    
+
 };
